afbe6de6b7aee64489b41f9c3902354e81109bf7
Luiz Felipe Liquer <liquer84@gmail.com>
2025-10-16 20:02:28 -0300

    banco

--- src/conectabairro/obj/Debug/net8.0/conectabairro.AssemblyInfo.cs
+++ src/conectabairro/obj/Debug/net8.0/conectabairro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("conectabairro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a06bbd98deeea479321a36d6bc3e11e8fc0e1aa1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba20b7c072221e99930959f1c943325a1bfefd12")]
 [assembly: System.Reflection.AssemblyProductAttribute("conectabairro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("conectabairro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
