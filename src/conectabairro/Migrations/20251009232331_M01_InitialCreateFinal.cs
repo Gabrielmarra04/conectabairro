fb2a5140574950c0259366e4cb750c031add873d
João Vitor Macedo <1593542@sga.pucminas.br>
2025-10-16 20:39:03 -0300

    teste joao

--- src/conectabairro/Migrations/20251009232331_M01_InitialCreateFinal.cs
+++ src/conectabairro/Migrations/20251009232331_M01_InitialCreateFinal.cs
@@ -17,7 +17,7 @@ namespace conectabairro.Migrations
                     UsuarioId = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
                     Nome = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
-                    Email = table.Column<string>(type: "nvarchar(50)", maxLength: 50,nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
                     PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: false),
                     Telefone = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     Rua = table.Column<string>(type: "nvarchar(max)", nullable: false),
@@ -37,12 +37,6 @@ namespace conectabairro.Migrations
                 table: "Usuario",
                 columns: new[] { "UsuarioId", "Bairro", "Cidade", "Cnpj", "Email", "Estado", "Nome", "PasswordHash", "RazaoSocial", "Rua", "Telefone", "TipoUsuarios" },
                 values: new object[] { 1, "N/A", "N/A", null, "admin@conectabairro.com", "N/A", "Administrador Geral", "$2b$13$nVGSJP0ybARQ3Y9TfhSKAeueSW8GsraU3KFfvyzatwwrAA7Im2.4G", null, "N/A", "", 0 });
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Usuario_Email",
-                table: "Usuario",
-                column: "Email",
-                unique: true); 
         }
 
         /// <inheritdoc />
