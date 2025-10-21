7565122c5f99e8b69e7a192f4aa4bdbacf8406d5
Luiz Felipe Liquer <liquer84@gmail.com>
2025-10-20 22:09:56 -0300

    Ajustando conflitos

--- src/conectabairro/obj/Release/net8.0/conectabairro.AssemblyInfo.cs
+++ src/conectabairro/obj/Release/net8.0/conectabairro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("conectabairro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb2a5140574950c0259366e4cb750c031add873d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e8d03f3b3e7140ac7c0948c01781e0a26f6cf60")]
 [assembly: System.Reflection.AssemblyProductAttribute("conectabairro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("conectabairro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
