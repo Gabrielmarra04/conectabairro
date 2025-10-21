c18fb93bf652e838bf21e46c1867b6b1d77a1079
João Vitor Macedo <1593542@sga.pucminas.br>
2025-10-20 21:53:16 -0300

    Teste Banco de Dados

--- src/conectabairro/obj/Debug/net8.0/conectabairro.AssemblyInfo.cs
+++ src/conectabairro/obj/Debug/net8.0/conectabairro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("conectabairro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29e1c318ebe5e12635a7249a5f974659291d9fb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c14037602adf1c56d640d28db870a510408115e")]
 [assembly: System.Reflection.AssemblyProductAttribute("conectabairro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("conectabairro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
