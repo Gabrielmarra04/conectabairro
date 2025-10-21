3cf54e687aba239cccad25f2ea669ab87e3a7ff0
João Vitor Macedo <1593542@sga.pucminas.br>
2025-10-20 22:13:09 -0300

    Alterações teste Git

--- src/conectabairro/obj/Debug/net8.0/conectabairro.AssemblyInfo.cs
+++ src/conectabairro/obj/Debug/net8.0/conectabairro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("conectabairro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c14037602adf1c56d640d28db870a510408115e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c18fb93bf652e838bf21e46c1867b6b1d77a1079")]
 [assembly: System.Reflection.AssemblyProductAttribute("conectabairro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("conectabairro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
