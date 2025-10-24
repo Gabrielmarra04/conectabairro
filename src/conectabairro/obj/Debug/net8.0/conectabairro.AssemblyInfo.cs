7045be545b6e16b5c2a66919a91c33eff3dbac4e
Luiz Felipe Liquer <liquer84@gmail.com>
2025-10-24 00:56:51 -0300

    Correção na tela MeusDados

--- src/conectabairro/obj/Debug/net8.0/conectabairro.AssemblyInfo.cs
+++ src/conectabairro/obj/Debug/net8.0/conectabairro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("conectabairro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2545cf2def425ccf1513be741940f4cb9b34c3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cc45877f244ab141e5e22bfcf1e0273699ac61b")]
 [assembly: System.Reflection.AssemblyProductAttribute("conectabairro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("conectabairro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
