86c13d1a0dcb9522ce778c795cb88ca0fc45c310
João Vitor Macedo <1593542@sga.pucminas.br>
2025-11-15 09:58:23 -0300

    Correção das pastas alteradas para efetuar o deploy

---
+++ src/conectabairro/Migrations/20251113225439_teste.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace conectabairro.Migrations
+{
+    /// <inheritdoc />
+    public partial class teste : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Reacao_PostId",
+                table: "Reacao");
+
+            migrationBuilder.UpdateData(
+                table: "Usuario",
+                keyColumn: "UsuarioId",
+                keyValue: 1,
+                column: "PasswordHash",
+                value: "$2a$13$qWvN8ZENXpEfrl6N9Mv34.V91a68hEXuqodm8q9WhQUh8lJFjKIcu");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reacao_PostId_AutorUsuarioId",
+                table: "Reacao",
+                columns: new[] { "PostId", "AutorUsuarioId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Reacao_PostId_AutorUsuarioId",
+                table: "Reacao");
+
+            migrationBuilder.UpdateData(
+                table: "Usuario",
+                keyColumn: "UsuarioId",
+                keyValue: 1,
+                column: "PasswordHash",
+                value: "$2a$13$shwrzAdaQXuYGGfolQlWI.avL5M/TIEDl/3oGiXVG.d6V7DFlBaci");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reacao_PostId",
+                table: "Reacao",
+                column: "PostId");
+        }
+    }
+}
